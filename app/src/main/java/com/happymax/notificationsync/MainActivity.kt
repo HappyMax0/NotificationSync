5ad058e66618bfe988d6835c27d35610ad3277a7
HappyMax0 <Jihaoran12@hotmail.com>
2024-08-01 08:15:35 +0800

    Add Searchbar

--- app/src/main/java/com/happymax/notificationsync/MainActivity.kt
+++ app/src/main/java/com/happymax/notificationsync/MainActivity.kt
@@ -44,6 +44,7 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -52,6 +53,8 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
@@ -755,6 +758,55 @@ fun ResetScreen(sharedPreferences: SharedPreferences, viewModel: MainScreenViewM
                 }
             }
         )
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun EmbeddedSearchBar(
+    onQueryChange: (String) -> Unit,
+    isSearchActive: Boolean,
+    onActiveChanged: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+    onSearch: ((String) -> Unit),
+) {
+    var searchQuery by rememberSaveable { mutableStateOf("") }
+    // 1
+    val activeChanged: (Boolean) -> Unit = { active ->
+        searchQuery = ""
+        onQueryChange("")
+        onActiveChanged(active)
+    }
+    SearchBar(
+        query = searchQuery,
+        // 2
+        onQueryChange = { query ->
+            searchQuery = query
+            onQueryChange(query)
+        },
+        // 3
+        onSearch = onSearch,
+        active = isSearchActive,
+        onActiveChange = activeChanged,
+        // 4
+        modifier = modifier
+            .padding(start = 12.dp, top = 2.dp, end = 12.dp, bottom = 12.dp)
+            .fillMaxWidth(),
+        placeholder = { Text("Search") },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Rounded.Search,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.onSurfaceVariant,
+            )
+        },
+        // 5
+        colors = SearchBarDefaults.colors(
+            containerColor = MaterialTheme.colorScheme.surfaceContainerLow,
+        ),
+        tonalElevation = 0.dp,
+    ) {
+        // Search suggestions or results
     }
 }
 
